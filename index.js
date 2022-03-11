05e591fe4f47f288d41b97c7bed0be301fe0ba28
Rodrigo <rodrigolfranca@gmail.com>
2022-03-11 15:56:15 -0300

    Cartão Finalizado, por enquanto

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use('/', express.static('src/home'));
-app.use('/aboutme', express.static('src/aboutme'));
+app.use('/aboutme', express.static('src/aboutme/'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/aboutme/script.js
+++ src/aboutme/script.js
@@ -1,1 +1,9 @@
-const $ = require( "jquery" )( window );
+fetch('https://simple-market-backend.herokuapp.com/aboutme')
+  .then(response => response.json())
+  .then(data => {
+      $('#cardText').append(`"${data.aboutMe}" -Leonardo Pimentel`);
+      $("#a-linkedin").attr("href", `${data.linkedin}`);
+      $("#a-linkedin").attr("href", `mailto:${data.email}`);
+      let phone = data.mobileNumber.split('+')[1];      
+      $("#a-linkedin").attr("href", `https://wa.me/${phone}`);
+  });
